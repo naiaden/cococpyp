d9f95234330f73b696efc7bc45a1721c7de6ebf8
louis <louis@naiaden.nl>
2014-05-12 23:06:48 +0200

    Bug gone? Onto the next one...

--- hpyplm/hpyplm.cc
+++ hpyplm/hpyplm.cc
@@ -22,24 +22,17 @@
 using namespace std;
 using namespace cpyp;
 
-int main(int argc, char** argv)
-{
-        if (argc != 4)
-        {
-                cerr << argv[0]
-                                << " <training_dir> <test_dir> <nsamples>\n\nEstimate a "
-                                << kORDER
-                                << "-gram HPYP LM and report perplexity\n100 is usually sufficient for <nsamples>\n";
-                return 1;
-        }
-        MT19937 eng(123456);
-        string train_input_directory = argv[1];
-        string test_file = argv[2];
-        int samples = atoi(argv[3]);
+int main(int argc, char** argv) {
+	if (argc != 4) {
+		cerr << argv[0] << " <training_dir> <test_dir> <nsamples>\n\nEstimate a " << kORDER << "-gram HPYP LM and report perplexity\n100 is usually sufficient for <nsamples>\n";
+		return 1;
+	}
+	MT19937 eng(123456);
+	string train_input_directory = argv[1];
+	string test_file = argv[2];
+	int samples = atoi(argv[3]);
 
-
-
-        ClassEncoder _class_encoder = ClassEncoder();
+	ClassEncoder _class_encoder = ClassEncoder();
 	ClassDecoder _class_decoder = ClassDecoder();
 
 	PatternModelOptions _pattern_model_options = PatternModelOptions();
@@ -50,146 +43,129 @@ int main(int argc, char** argv)
 	_pattern_model_options.QUIET = true;
 	_pattern_model_options.MINTOKENS = 1;
 
+	boost::filesystem::path background_dir(train_input_directory);
+	boost::filesystem::directory_iterator bit(background_dir), beod;
 
-        boost::filesystem::path background_dir(train_input_directory);
-        boost::filesystem::directory_iterator bit(background_dir), beod;
-
-        std::vector<std::string> train_input_files;
-        BOOST_FOREACH(boost::filesystem::path const &p, std::make_pair(bit, beod))
-        {
-                if(is_regular_file(p) && p.extension() == ".txt")
-                {
-                        train_input_files.push_back(p.string());
-                }
-        }
-
-        std::cout << "Found " << train_input_files.size() << " files" << std::endl;
-
-        _class_encoder.build(train_input_files, true);
-        _class_encoder.save("/tmp/tmpout/cpyp.colibri.cls");
-
-        std::string dat_output_file = "/tmp/tmpout/cpyp.colibri.dat";
-
-        for (auto i : train_input_files)
-        {
-                _class_encoder.encodefile(i, dat_output_file, false, false, true, true);
-        }
-
-        _class_decoder.load("/tmp/tmpout/cpyp.colibri.cls");
-
-        IndexedCorpus _indexed_corpus = IndexedCorpus(dat_output_file);
-
-        PatternModel<uint32_t> _pattern_model = PatternModel<uint32_t>(&_indexed_corpus);
-        _pattern_model.train(dat_output_file, _pattern_model_options, nullptr);
-
-        std::cout << ">> maxn:" << _pattern_model.maxlength() << std::endl;
-    
-        cerr << "Reading corpus...\n";
-        cerr << "E-corpus size: " << _indexed_corpus.sentences() << " sentences\t ("
-        		<< _pattern_model.types() << " word types (bugged), " << _pattern_model.size() << " patterns types and " << _pattern_model.tokens() << " word tokens)\n";
-
-
-        PYPLM<kORDER> lm(_pattern_model.size(), 1, 1, 1, 1);
-        for (int sample = 0; sample < samples; ++sample)
-        {
-        		int p_iter = 0;
-				for (IndexPattern it : _indexed_corpus) {
-					std::cout << p_iter++ << std::endl;
-					std::vector<Pattern> result = _pattern_model.getreverseindex(it.ref);
-					for (Pattern q : result) {
-						//for(Pattern p : _pattern_model.getreverseindex(it.ref)) {
-						size_t p_size = q.size();
-
-		//						if(p_size == kORDER)
-		//						{
-
-						Pattern context = Pattern();
-						Pattern focus = Pattern();
-						if (p_size > 0) {
-							if (p_size == 1) {
-								focus = q[0];
-							} else {
-								context = Pattern(q, 0, p_size - 1);
-								focus = q[p_size - 1];
-							}
-		//							std::cout << context.tostring(_class_decoder) << " .. " << focus.tostring(_class_decoder) << std::endl;
-
-							if(sample > 0) { std::cout << ">>" << q.tostring(_class_decoder) << " c:[" << context.tostring(_class_decoder) << "] f:[" << focus.tostring(_class_decoder) << "]" << std::endl; }
-                     ClassDecoder* cd = nullptr;
-                     if (sample > 0) cd = &_class_decoder;
-							if (sample > 0)
-								lm.decrement(focus, context, eng, cd);
-							lm.increment(focus, context, eng, &_class_decoder);
-						}
-                  else {
-                    std::cout << "Skipping: " << q.tostring(_class_decoder) << std::endl;
-                  }
-		//						}
-					}
-				}
-
-                if (sample % 10 == 9)
-                {
-                        cerr << " [LLH=" << lm.log_likelihood() << "]" << endl;
-                        if (sample % 30u == 29)
-                                lm.resample_hyperparameters(eng);
-                }
-                else
-                {
-                        cerr << '.' << flush;
-                }
-        }
-
-        std::cout << "Done for now" << std::endl;
-		exit(4);
-
-
-
-
-
-
-
-
-/*
-
-		vector<vector<unsigned> > test;
-		ReadFromFile(test_file, &dict, &test, &tv);
-
-
-        double llh = 0;
-        unsigned cnt = 0;
-        unsigned oovs = 0;
-        for (auto& s : test)
-        {
-//                ctx.resize(kORDER - 1);
-//                for (unsigned i = 0; i <= s.size(); ++i)
-//                {
-//                        unsigned w = (i < s.size() ? s[i] : kEOS);
-//                        double lp = log(lm.prob(w, ctx)) / log(2);
-//                        if (i < s.size() && vocabe.count(w) == 0)
-//                        {
-//                                cerr << "**OOV ";
-//                                ++oovs;
-//                                lp = 0;
-//                        }
-//                        cerr << "p(" << dict.Convert(w) << " |";
-//                        for (unsigned j = ctx.size() + 1 - kORDER; j < ctx.size(); ++j)
-//                                cerr << ' ' << dict.Convert(ctx[j]);
-//                        cerr << ") = " << lp << endl;
-//                        ctx.push_back(w);
-//                        llh -= lp;
-//                        cnt++;
-//                }
-        }
-        cnt -= oovs;
-        cerr << "  Log_10 prob: " << (-llh * log(2) / log(10)) << endl;
-        cerr << "        Count: " << cnt << endl;
-        cerr << "         OOVs: " << oovs << endl;
-        cerr << "Cross-entropy: " << (llh / cnt) << endl;
-        cerr << "   Perplexity: " << pow(2, llh / cnt) << endl;
-        return 0;
-*/
+	std::vector<std::string> train_input_files;
+	BOOST_FOREACH(boost::filesystem::path const &p, std::make_pair(bit, beod)){
+	if(is_regular_file(p) && p.extension() == ".txt")
+	{
+		train_input_files.push_back(p.string());
+	}
 }
 
+	std::cout << "Found " << train_input_files.size() << " files" << std::endl;
 
+	_class_encoder.build(train_input_files, true);
+	_class_encoder.save("/tmp/tmpout/cpyp.colibri.cls");
+
+	std::string dat_output_file = "/tmp/tmpout/cpyp.colibri.dat";
+
+	for (auto i : train_input_files) {
+		_class_encoder.encodefile(i, dat_output_file, false, false, true, true);
+	}
+
+	_class_decoder.load("/tmp/tmpout/cpyp.colibri.cls");
+
+	IndexedCorpus _indexed_corpus = IndexedCorpus(dat_output_file);
+
+	PatternModel<uint32_t> _pattern_model = PatternModel<uint32_t>(&_indexed_corpus);
+	_pattern_model.train(dat_output_file, _pattern_model_options, nullptr);
+
+	std::cout << ">> maxn:" << _pattern_model.maxlength() << std::endl;
+
+	cerr << "Reading corpus...\n";
+	cerr << "E-corpus size: " << _indexed_corpus.sentences() << " sentences\t (" << _pattern_model.types() << " word types (bugged), " << _pattern_model.size()
+			<< " patterns types and " << _pattern_model.tokens() << " word tokens)\n";
+
+	PYPLM<kORDER> lm(_pattern_model.size(), 1, 1, 1, 1);
+	for (int sample = 0; sample < samples; ++sample) {
+		int p_iter = 0;
+		for (IndexPattern it : _indexed_corpus) {
+			//std::cout << p_iter++ << std::endl;
+			std::vector<Pattern> result = _pattern_model.getreverseindex(it.ref);
+			for (Pattern q : result) {
+				//for(Pattern p : _pattern_model.getreverseindex(it.ref)) {
+				size_t p_size = q.size();
+
+				Pattern context = Pattern();
+				Pattern focus = Pattern();
+				if (p_size > 0) {
+					if (p_size == 1) {
+						focus = q[0];
+					} else {
+						context = Pattern(q, 0, p_size - 1);
+						focus = q[p_size - 1];
+					}
+
+					if (sample > 0) {
+						std::cout << ">>" << q.tostring(_class_decoder) << " c:[" << context.tostring(_class_decoder) << "] f:[" << focus.tostring(_class_decoder) << "]"
+								<< std::endl;
+					}
+
+					ClassDecoder* cd = nullptr;
+					if (sample > 0)
+						cd = &_class_decoder;
+
+					if (sample > 0)
+						lm.decrement(focus, context, eng, cd);
+					lm.increment(focus, context, eng, &_class_decoder);
+				} else {
+					std::cout << "Skipping: " << q.tostring(_class_decoder) << std::endl;
+				}
+			}
+		}
+
+		if (sample % 10 == 9) {
+			cerr << " [LLH=" << lm.log_likelihood() << "]" << endl;
+			if (sample % 30u == 29)
+				lm.resample_hyperparameters(eng);
+		} else {
+			cerr << '.' << flush;
+		}
+	}
+
+	std::cout << "Done for now" << std::endl;
+	exit(4);
+
+	/*
+
+	 vector<vector<unsigned> > test;
+	 ReadFromFile(test_file, &dict, &test, &tv);
+
+
+	 double llh = 0;
+	 unsigned cnt = 0;
+	 unsigned oovs = 0;
+	 for (auto& s : test)
+	 {
+	 //                ctx.resize(kORDER - 1);
+	 //                for (unsigned i = 0; i <= s.size(); ++i)
+	 //                {
+	 //                        unsigned w = (i < s.size() ? s[i] : kEOS);
+	 //                        double lp = log(lm.prob(w, ctx)) / log(2);
+	 //                        if (i < s.size() && vocabe.count(w) == 0)
+	 //                        {
+	 //                                cerr << "**OOV ";
+	 //                                ++oovs;
+	 //                                lp = 0;
+	 //                        }
+	 //                        cerr << "p(" << dict.Convert(w) << " |";
+	 //                        for (unsigned j = ctx.size() + 1 - kORDER; j < ctx.size(); ++j)
+	 //                                cerr << ' ' << dict.Convert(ctx[j]);
+	 //                        cerr << ") = " << lp << endl;
+	 //                        ctx.push_back(w);
+	 //                        llh -= lp;
+	 //                        cnt++;
+	 //                }
+	 }
+	 cnt -= oovs;
+	 cerr << "  Log_10 prob: " << (-llh * log(2) / log(10)) << endl;
+	 cerr << "        Count: " << cnt << endl;
+	 cerr << "         OOVs: " << oovs << endl;
+	 cerr << "Cross-entropy: " << (llh / cnt) << endl;
+	 cerr << "   Perplexity: " << pow(2, llh / cnt) << endl;
+	 return 0;
+	 */
+}
 
--- hpyplm/hpyplm.h
+++ hpyplm/hpyplm.h
@@ -22,99 +22,120 @@
 
 namespace cpyp {
 
-template <unsigned N> struct PYPLM;
+template<unsigned N> struct PYPLM;
 
 template<> struct PYPLM<0> : public UniformVocabulary {
-  PYPLM(unsigned vs, double a, double b, double c, double d) :
-    UniformVocabulary(vs, a, b, c, d) {}
+	PYPLM(unsigned vs, double a, double b, double c, double d) :
+			UniformVocabulary(vs, a, b, c, d) {
+	}
 };
 
 // represents an N-gram LM
-template <unsigned N> struct PYPLM {
-  PYPLM() :
-      backoff(0,1,1,1,1),
-      tr(1,1,1,1,0.8,0.0) {}
-  explicit PYPLM(unsigned vs, double da = 1.0, double db = 1.0, double ss = 1.0, double sr = 1.0) :
-      backoff(vs, da, db, ss, sr),
-      tr(da, db, ss, sr, 0.8, 0.0) {}
-  template<typename Engine>
-  void increment(const Pattern& w, const Pattern& context, Engine& eng, ClassDecoder *const decoder) {
-    const double bo = backoff.prob(w, context);
+template<unsigned N> struct PYPLM {
+	PYPLM() :
+			backoff(0, 1, 1, 1, 1), tr(1, 1, 1, 1, 0.8, 0.0) {
+	}
+	explicit PYPLM(unsigned vs, double da = 1.0, double db = 1.0, double ss = 1.0, double sr = 1.0) :
+			backoff(vs, da, db, ss, sr), tr(da, db, ss, sr, 0.8, 0.0) {
+	}
+	template<typename Engine>
+	void increment(const Pattern& w, const Pattern& context, Engine& eng, ClassDecoder * const decoder) {
+		const double bo = backoff.prob(w, context);
 
-    Pattern pattern = Pattern(context, context.size()-1-(N-1), context.size()-1);
-    pattern.reverse();
+		Pattern pattern = Pattern(context, context.size() - 1 - (N - 1), N-1);
+//		Pattern pattern = Pattern(context, context.size() - 1 - (N - 1), context.size() - 1);
+		pattern = pattern.reverse();
 
-    //if(decoder != nullptr) {
-    std::string cs = context.tostring(*decoder);
-    std::string fs = w.tostring(*decoder);
 
-    auto it = p.find(pattern);
-    if(it == p.end()) {
-    	it = p.insert(make_pair(pattern, crp<Pattern>(0.8,0))).first;
-    	tr.insert(&it->second); // add to resampler
-    }
-    if (it->second.increment(w, bo, eng, cs == "o.a. uit" && fs == ":"))
-    {
-        
-                    //if(decoder != nullptr) std::cout << "bo(" << N << ")++\t\tc:[" << w.tostring(*decoder) << "] c:[" << context.tostring(*decoder) << "]" << std::cout;
-    	backoff.increment(w, context, eng, decoder);
-    }
-    if (cs == "o.a. uit" && fs == ":") { 
-       
-        do {
-               std::cout << '\n' <<"Press the Enter key to continue.";
-              } while (std::cin.get() != '\n');
 
-    }
+		//if(decoder != nullptr) {
+		std::string cs = context.tostring(*decoder);
+		std::string fs = w.tostring(*decoder);
+		std::string ps = pattern.tostring(*decoder);
 
-  }
+		auto it = p.find(pattern);
+		if (it == p.end()) {
+			it = p.insert(make_pair(pattern, crp<Pattern>(0.8, 0))).first;
+			tr.insert(&it->second); // add to resampler
+		}
 
-  template<typename Engine>
-  void decrement(const Pattern& w, const Pattern& context, Engine& eng, ClassDecoder *const decoder) {
-          Pattern pattern = Pattern(context, context.size()-1-(N-1), context.size()-1);
-	  pattern.reverse();
+		if (fs == ":" && cs == "o.a. uit") {
+			std::cout << "[" << cs << "/" << ps << "] +++    " << it->second.num_customers(w) << std::endl;
+		}
 
-	  auto it = p.find(pattern);
-	  assert(it != p.end());
-        if(decoder != nullptr) std::cout << "de(" << N << ")--\t\tf:[" << w.tostring(*decoder) << "] c:[" << context.tostring(*decoder) << "]" << std::endl;
-	  if(it->second.decrement(w, eng))
-          {
-                    if(decoder != nullptr) std::cout << "X" << std::endl;
-                    //if(decoder != nullptr) std::cout << "bo(" << N << ")--\t\tc:[" << w.tostring(*decoder) << "] c:[" << context.tostring(*decoder) << "]" << std::cout;
-		  backoff.decrement(w, context, eng, decoder);
-		  //backoff.decrement(w, context, eng, nullptr);
-        }
-  }
+		if (it->second.increment(w, bo, eng, false/*cs == "o.a. uit" && fs == ":"*/)) {
+			backoff.increment(w, context, eng, decoder);
+		}
 
-  double prob(const Pattern& w, const Pattern& context) const {
-	  const double bo = backoff.prob(w, context);
+		//if (cs == "o.a. uit" && fs == ":") {
+		//
+		//    do {
+		//           std::cout << '\n' <<"Press the Enter key to continue.";
+		//          } while (std::cin.get() != '\n');
 
-	  Pattern pattern = Pattern(context, context.size()-1-(N-1), context.size()-1);
-	  pattern.reverse();
+		//}
 
-	  auto it = p.find(pattern);
-	  if(it == p.end()) return bo;
-	  return it->second.prob(w,bo);
-  }
+	}
 
-  double log_likelihood() const {
-    return backoff.log_likelihood() + tr.log_likelihood();
-  }
+	template<typename Engine>
+	void decrement(const Pattern& w, const Pattern& context, Engine& eng, ClassDecoder * const decoder) {
+		Pattern pattern = Pattern(context, context.size() - 1 - (N - 1), N-1);
+//		Pattern pattern = Pattern(context, context.size() - 1 - (N - 1), context.size() - 1);
+		pattern = pattern.reverse();
 
-  template<typename Engine>
-  void resample_hyperparameters(Engine& eng) {
-    tr.resample_hyperparameters(eng);
-    backoff.resample_hyperparameters(eng);
-  }
+		auto it = p.find(pattern);
+		assert(it != p.end());
+		//if(decoder != nullptr) std::cout << "de(" << N << ")--\t\tf:[" << w.tostring(*decoder) << "] c:[" << context.tostring(*decoder) << "]" << std::endl;
 
-  template<class Archive> void serialize(Archive& ar, const unsigned int version) {
-    backoff.serialize(ar, version);
-    ar & p;
-  }
+		std::string fs = "";
+		std::string cs = context.tostring(*decoder);
+		if (decoder != nullptr) {
+			fs = w.tostring(*decoder);
+		}
 
-  PYPLM<N-1> backoff;
-  tied_parameter_resampler<crp<Pattern>> tr;
-  std::unordered_map<const Pattern, crp<Pattern>> p;  // .first = context .second = CRP
+		if (fs == ":" && cs == "o.a. uit") {
+			std::cout << "   --- " << it->second.num_customers(w) << std::endl;
+		}
+
+		if (it->second.decrement(w, eng)) {
+			if (decoder != nullptr)
+				std::cout << "X" << std::endl;
+			//if(decoder != nullptr) std::cout << "bo(" << N << ")--\t\tc:[" << w.tostring(*decoder) << "] c:[" << context.tostring(*decoder) << "]" << std::cout;
+			backoff.decrement(w, context, eng, decoder);
+			//backoff.decrement(w, context, eng, nullptr);
+		}
+	}
+
+	double prob(const Pattern& w, const Pattern& context) const {
+		const double bo = backoff.prob(w, context);
+
+		Pattern pattern = Pattern(context, context.size() - 1 - (N - 1), context.size() - 1);
+		pattern.reverse();
+
+		auto it = p.find(pattern);
+		if (it == p.end())
+			return bo;
+		return it->second.prob(w, bo);
+	}
+
+	double log_likelihood() const {
+		return backoff.log_likelihood() + tr.log_likelihood();
+	}
+
+	template<typename Engine>
+	void resample_hyperparameters(Engine& eng) {
+		tr.resample_hyperparameters(eng);
+		backoff.resample_hyperparameters(eng);
+	}
+
+	template<class Archive> void serialize(Archive& ar, const unsigned int version) {
+		backoff.serialize(ar, version);
+		ar & p;
+	}
+
+	PYPLM<N - 1> backoff;
+	tied_parameter_resampler<crp<Pattern>> tr;
+	std::unordered_map<const Pattern, crp<Pattern>> p;  // .first = context .second = CRP
 };
 
 }
