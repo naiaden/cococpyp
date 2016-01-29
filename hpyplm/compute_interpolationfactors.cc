0c6b6972b51cf7bc6ea855a3fc0e89279572799c
louis <louis@naiaden.nl>
2016-01-29 22:57:32 +0100

    much speed wow

--- hpyplm/compute_interpolationfactors.cc
+++ hpyplm/compute_interpolationfactors.cc
@@ -15,6 +15,14 @@
 
 #include "cmdline.h"
 
+struct PatternComp
+{
+    bool operator() (const Pattern& lhs, const Pattern& rhs) const
+    {
+        return lhs>rhs;
+    }
+};
+
 int main(int argc, char** argv) 
 {
     cmdline::parser clp;
@@ -23,14 +31,12 @@ int main(int argc, char** argv)
     clp.add<std::string>("output", 'o', "output directory", true);
     clp.add<std::string>("trainmodel", 'm', "the name of the training model", true);
     clp.add<int>("n", 'n', "n", false, 4);
-    clp.add<int>("parallel", 'p', "number of parallel bla", false);
 
     clp.parse_check(argc, argv);
 
     std::string _input_directory = clp.get<std::string>("trainoutput");
     std::string _output_directory = clp.get<std::string>("output");
     std::string _input_run_name = clp.get<std::string>("trainmodel");
-    int _parallel_bla = clp.get<int>("parallel");
 
     ClassEncoder _class_encoder = ClassEncoder();
     ClassDecoder _class_decoder = ClassDecoder();
@@ -49,7 +55,7 @@ int main(int argc, char** argv)
    std::string _input_patternmodel_file_name = _base_input_name + ".patternmodel";
 
    std::string _general_base_output_name = _output_directory + "/" + _input_run_name;
-   std::string _general_interpolationfactors_output_name = _general_base_output_name + ".factors." + std::to_string(_parallel_bla);
+   std::string _general_interpolationfactors_output_name = _general_base_output_name + ".factors";
 
    std::ofstream _general_output;
    _general_output.open(_general_interpolationfactors_output_name);
@@ -63,57 +69,53 @@ int main(int argc, char** argv)
    std::cout << "Done loading indexed corpus" << std::endl;
    
    PatternModel<uint32_t> _train_pattern_model(_input_patternmodel_file_name, _pattern_model_options, nullptr, &_indexed_corpus);
-   PatternSet<uint64_t> allPatterns = _train_pattern_model.extractset(4,4);
    PatternSet<uint64_t> allWords = _train_pattern_model.extractset(1,1);
     std::cout << "Done creating pattern set" << std::endl;
 
-    PatternSet<uint64_t> set_of_all_contexts = _train_pattern_model.extractset(1,1);//allWords;
-    PatternSet<uint64_t> set_of_contexts = PatternSet<uint64_t>();
-
     int bla = 0;
-    for(int n = 1; n < 4; ++n)
+    for(int n = 3; n <= 4; ++n)
     {
-        std::cout << "Generating contexts with length " << n << std::endl;
-        for(auto context : set_of_all_contexts)
-        {
-            if(context.hash() % 28 == _parallel_bla)
-            {
-                ++bla;
+        
+        PatternSet<uint64_t> allPatterns = _train_pattern_model.extractset(n,n);
+        std::cout << "Done extracting set for " << n << std::endl;
+        
+        std::set<Pattern, PatternComp> ordered_patterns;
 
-                int sum = 0;
-                int triggers = 0;
-                std::vector<int> occs;
-                for(auto word : allWords)
+        for(auto pattern : allPatterns)
+        {
+            ordered_patterns.insert(pattern);
+        }
+        std::cout << "Done ordering the set" << std::endl;
+
+        Pattern previous_prefix = Pattern();
+        double llh = 0;
+        int sum = 0;
+        std::vector<int> added_patterns = std::vector<int>();
+        for(auto pattern: ordered_patterns)
+        {
+            Pattern prefix = Pattern(pattern, 0, n-1);
+            if(prefix != previous_prefix)
+            {
+
+                for(auto count : added_patterns)
                 {
-                    const Pattern ngram = context + word;
-                    int count = _train_pattern_model.occurrencecount(ngram);
-                    if(count)
-                    {
-                        occs.push_back(count);
-                        ++triggers;
-                        sum += count;
-                        set_of_contexts.insert(ngram);
-                        //std::cout << "Adding " << context.tostring(_class_decoder) << std::endl;
-                        // possible optimisation: only insert context once
-                    }
-                }
-                
-                double llh = 0;
-                for(auto occ : occs)
-                {
-                    double mle = occ*1.0/sum;
+                    double mle = count*1.0/sum;
                     llh -= log(mle);
                 }
+    
+                std::cout << prefix.tostring(_class_decoder) << "\t" << added_patterns.size() << "\t" << -llh << "\t" << llh/added_patterns.size() << std::endl;
+                std::cout << "----------------------------------------" << std::endl;
+                previous_prefix = prefix;
 
-                _general_output << context.tostring(_class_decoder) << "\t" << triggers << "\t" << -llh << "\t" << llh/triggers << std::endl;
+                llh = 0;
+                sum = 0;
+                added_patterns = std::vector<int>();
             }
+            std::cout << "[" << prefix.tostring(_class_decoder) << "]\t" << pattern.tostring(_class_decoder) << std::endl;
+            int count = _train_pattern_model.occurrencecount(pattern);
+            sum += count;
+            added_patterns.push_back(count);
         }
-
-        set_of_all_contexts = set_of_contexts;
-        set_of_contexts = PatternSet<uint64_t>();
-
-        std::cout << _parallel_bla << "]: Processed " << bla << "contexts for " << n << std::endl;
-        break;
     }
 
    _general_output.close();
